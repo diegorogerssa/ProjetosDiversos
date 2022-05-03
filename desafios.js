bea33a518e9079e1db4c577574625a20f96bf30a
Rogers <rogersmhdg@gmail.com>
2022-05-03 20:19:55 -0300

    exerciocio-11

--- desafios.js
+++ desafios.js
@@ -297,7 +297,7 @@ function techList(tec, yourName) {
 	}
 	return oAlfabetica;
 }   
-console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Giulia'));
+//console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Giulia'));
     
 // Exemplo:
 // Entradas da função:
@@ -338,11 +338,31 @@ console.log(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Giulia'));
 // Exemplo: caso o parâmetro da função seja [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1], generatePhoneNumber deverá retornar (12) 34567-8901.
 // Se a função receber um array com tamanho diferente de 11, a mesma deve retornar "Array com tamanho incorreto.".
 // Caso algum dos números da array seja menor que 0, maior que 9 ou se repita 3 vezes ou mais, generatePhoneNumber deverá retornar a string "não é possível gerar um número de telefone com esses valores".
-    
-function generatePhoneNumber() {
+
+function generatePhoneNumber(numberTel) {
 	// seu código aqui
+	
+
+	if (numberTel.length > 11 || numberTel.length < 11){
+		return 'Array com tamanho incorreto.';
+	}else {
+		for (let indice = 0; indice < numberTel.length; indice+=1) {
+			let contarNumero = numberTel[indice];
+			let cont = 0;
+			for(let i of numberTel){
+				if (contarNumero === i){
+					cont += 1;
+					if (cont >= 3 || contarNumero < 0 || contarNumero > 9) {
+						return 'não é possível gerar um número de telefone com esses valores';
+					}
+				}
+			}	
+		}	
+	}
+	let tel = `(${numberTel[0]}${numberTel[1]}) ${numberTel[2]}${numberTel[3]}${numberTel[4]}${numberTel[5]}${numberTel[6]}-${numberTel[7]}${numberTel[8]}${numberTel[9]}${numberTel[10]}`;
+	return tel;
 }
-    
+console.log(generatePhoneNumber([1, 2, 3, 4, 5, 7, 7, 8, 5, 4, 1]));
 // O que será verificado:
 // Retorne a string "Array com tamanho incorreto." caso o array tenha o tamanho diferente de 11
 // Retorne a string "não é possível gerar um número de telefone com esses valores" caso algum dos números do array seja menor que 0
